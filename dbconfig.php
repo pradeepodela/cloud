5314f87002c6523497e8578121ea891775b749ca
pradeepodela <72432569+pradeepodela@users.noreply.github.com>
2020-12-24 22:00:22 +0530

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,11 @@
+<?php
+/****************************************************/
+/************** Created by : Vivek Gupta ************/
+/***************     www.vsgupta.in     *************/
+/***************     www.iotmonk.com     *************/
+/****************************************************/ 
+define('DB_USER', "id15657342_cloud");     // Your database user name
+define('DB_PASSWORD', "@Pradeep9246");			// Your database password (mention your db password here)
+define('DB_DATABASE', "id15657342_iotcloud"); // Your database name
+define('DB_SERVER', "localhost");			// db server (Mostly will be 'local' host)
+?>
